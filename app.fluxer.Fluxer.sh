1efce5a0aabbd548db5098805f12cf71bf855b55
yofukashino <73281112+yofukashino@users.noreply.github.com>
2026-03-26 09:37:36 +0530

    feat: persistent flags

--- app.fluxer.Fluxer.sh
+++ app.fluxer.Fluxer.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-exec zypak-wrapper /app/fluxer/fluxer_desktop "$@"
+
+FLAGS_PATH="${XDG_CONFIG_HOME}/fluxer-flags.conf"
+
+if [ -f "${FLAGS_PATH}" ]; then
+    mapfile -t FLAGS <<< "$(grep -Ev '^\s*$|^#' "${FLAGS_PATH}")"
+fi
+
+exec zypak-wrapper /app/fluxer/fluxer_desktop "${FLAGS[@]}" "$@"
